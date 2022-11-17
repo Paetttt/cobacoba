095f6f7472c52b968ca77e6ee98abbca4e95ba73
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-11-17 14:50:55 +0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+#ini adalah percobaan pertama
+# print ("hello world")
+# print(12+110)
+# print (12/67)
+# print ("nanti ug nya susah hehehe")
+# print ("gpp kok kAK!!!!")
+# print (789/22)
+
+s_satu = int(input("Masukkan angka: "))
+s_dua = int(input("Masukkan angka: "))
+
+persegi = s_satu * s_dua
+
+print ("Luas persegi adalah", persegi)
